8fa27e59412ae27b290a5722850227172347fafd
Luciano Nunes de Siqueira <lnsiqueira@gmail.com>
2026-02-27 15:39:38 -0300

    add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:webapp_pesquisa_avalicao_dashboard/firebase_options.dart';
 import 'package:webapp_pesquisa_avalicao_dashboard/screens/login_screen.dart';
 import 'theme/app_theme.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
